0a797ee3ed182cb79cb6cfc4d7845a0357001fd6
joannehoazadobe <joaho@adobe.com>
2025-10-30 07:33:58 -0700

    more mods to content fragment block

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -51,6 +51,12 @@ export default async function decorate(block) {
           <p data-aue-prop="subject" data-aue-label="subject" data-aue-type="text" class='subject'>${
             cfReq?.subject
           }</p>
+          <p data-aue-prop="pretitle" data-aue-label="pretitle" data-aue-type="text" class='headline'>${
+            cfReq?.audiences[0].audienceType
+          }</p>
+          <p data-aue-prop="detail" data-aue-label="detail" data-aue-type="richtext" class='detail'>${
+            cfReq?.atAGlance?.html
+          }</p>
 
       </div>
       <div class='banner-logo'>
