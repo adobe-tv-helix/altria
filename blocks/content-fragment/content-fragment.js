c2075ece0abcd23cb22f56bf91344319aaa4a8c5
joannehoazadobe <joaho@adobe.com>
2025-10-30 07:49:34 -0700

    more mods to content fragment block. fix typo

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -55,7 +55,7 @@ export default async function decorate(block) {
             cfReq?.atAGlance?.html
           }</p>
           <p data-aue-prop="channels" data-aue-label="channels" class='channels'>${
-            cfReq?.channels.toUpperCase()
+            cfReq?.channels
           }</p>
           <p data-aue-label="sections-0" class='sections'>${cfReq?.section[0].label}${cfReq?.section[0].body?.html}</p>
       </div>
